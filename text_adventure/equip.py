affacabe47c5cbbbb02c844abcb6552661df1e6a
QuishyTehQuish <143221643+QuishyTehQuish@users.noreply.github.com>
2023-08-27 03:54:43 +0000

    Co-authored-by: Hunter Henry <FeFaxy@users.noreply.github.com>

---
+++ text_adventure/equip.py
@@ -0,0 +1,21 @@
+import random
+#variable = 'fucking crist'
+class Equipment:
+    equipment_list = [
+        {
+            "name": "Sword",
+            "description": "A sharp sword for combat.",
+        },
+        {
+            "name": "Shield",
+            "description": "A sturdy shield for defense.",
+        },
+        {
+            "name": "Potion",
+            "description": "A healing potion to restore health.",
+        },
+        # Add more equipment items as needed
+    ]
+
+#def something():
+#    print('text')
