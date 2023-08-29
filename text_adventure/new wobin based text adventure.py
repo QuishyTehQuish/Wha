943e1c55693a9dcb0b5a733cb728937113185be6
QuishyTehQuish <143221643+QuishyTehQuish@users.noreply.github.com>
2023-08-29 00:55:08 +0000

    made a clamp

--- text_adventure/new wobin based text adventure.py
+++ text_adventure/new wobin based text adventure.py
@@ -8,18 +8,31 @@ import spells_list
 import Event_list
 
 
+# python fuckin sucks
+def clamp(n,min,max):
+    e = [n,min,max]
+    print(e)
+    if min < n < max:
+        return n
+    elif n <= min:
+        return min
+    elif n >= max:
+        return max
+
 #list stored in objects so they can be init once and called repeatidly
 #items 
 #abilities
 
 # game variables
+player = 0
 progression = 0
 player_inventory = []    
 player_money = int(0)
     
 
 
-
+def _player_status_update():
+    player.stats = [player.level,player.exp,player.next_level,player.name,player.job,player.hp_max,player.hp_cur,player.atk,player.dfp,player.spd]
 
 
 def make_an_item():
@@ -27,22 +40,20 @@ def make_an_item():
 
 
 def event_manager():
+    global player
     choices = ["1",'2','3','4']
     choice = input("What will you do? 1:Adventure, 2:Manage inventory, 3:Status, 4:die")
     if choice in choices:
         match choice:
             case '1':
-                event = Event_list.new_event(progression,player)
+                event = Event_list.new_event(progression, player)
                 #print(event)
                 #print(type(event))
                 if isinstance(event,dict):
-                    print('here')
                     for i in event:
                         if 'items' in event[i]:
-                            print('ite')
                             gain_items = event[i]['items']
                             for i in gain_items:
-                                print('yoa')
                                 global player_inventory
                                 player_inventory.append(i)
                             print(f'You gained {gain_items}')
@@ -50,11 +61,23 @@ def event_manager():
                             gain_money = event[i]['money']
                             global player_money 
                             player_money += gain_money
-                            print(f'You gained {gain_money}')
+                            print(f'You gained {gain_money} money')
+                        elif 'exp' in event[i]:
+                            gain_exp = event[i]['exp']                            
+                            player.exp += gain_exp
+                            print(f'You gained {gain_exp} exp')
+                            if player.exp >= player.next_level:
+                                player.level += 1
+                                player.exp = clamp(player.next_level - player.exp,0,player.next_level)
+                                print(player.exp)
+                                player.next_level = int(player.next_level * 2)
+                                print(f'Your now level {player.level}')
+
 
             case '2':
                 print(f"Your inventory\n{player_inventory}\nYour money: {player_money}")
             case '3':
+                _player_status_update()
                 print(player.stats)
                 
             case '4':
